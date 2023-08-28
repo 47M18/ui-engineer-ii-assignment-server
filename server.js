4e8887053d1822e35c600cb95bc6fb70b4975b47
Almir Cibra <almir.cibra@coxautoinc.com>
2023-08-28 09:54:45 -0400

    Adds CORS support for client URL; fixes package-lock in gitignore

--- server.js
+++ server.js
@@ -1,14 +1,19 @@
-const express = require('express');
 const fs = require('node:fs');
+const cors = require('cors');
+const express = require('express');
 const app = express();
 const port = 3055;
 
-app.get('/', (req, res) => {
+app.use(cors({
+  origin: 'https://uie2-client.onrender.com'
+}))
+
+app.get('/', (_req, res) => {
   res.send('Hello World!')
 });
 
-app.get('/api/items', (req, res) => {
-  fs.readFile('./public/UIE-InterviewProject.json', 'utf-8', (e, data) => {
+app.get('/api/items', (_req, res) => {
+  fs.readFile('./public/UIE-InterviewProject.json', 'utf-8', (_e, data) => {
     res.json(JSON.parse(data));
   });
 });
